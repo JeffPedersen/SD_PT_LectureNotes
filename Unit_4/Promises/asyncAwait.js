70041da3df9c7befcf9b233c1f0ddb2c7f8b8ff6
Enho <pedersenofficial@gmail.com>
2023-03-12 14:13:50 -0400

    add async

--- Unit_4/Promises/asyncAwait.js
+++ Unit_4/Promises/asyncAwait.js
@@ -37,3 +37,19 @@ async function getName() {
 console.log(getName()); //Promise { 'Frodo' }
 // .then(): a method that attaches a callback function for the resolution of the promise
 getName().then((data) => console.log(data));
+
+/* //! Await
+        - Tells JS that a promise is being returned and to wait on that result
+*/
+
+async function extraData() {
+    // returnData() is in our promises.js
+    //let info = returnData(); //? Ran so quickly it printed [object Promise] 
+
+    // Include await so we "wait" for the response, so we dont get undefined data as above 
+    let info = await returnData();
+
+    return `${info} stored in some cloud... or API...`
+}
+
+extraData().then((data) => console.log("line 51 return of asyncAwait.js", data));
